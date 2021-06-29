22eb6dbdf8d17de976ce00bb66e42bc19d6c141d
Mr. Crank <crank19869@gmail.com>
2021-06-29 18:08:37 +0600

    yii2 init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=yii2shorturl_mysql_1;dbname=ShortUrl',
+    'username' => 'mysql',
+    'password' => 'root',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
