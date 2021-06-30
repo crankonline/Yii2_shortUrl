40a3b184aa896a2be76afd82ef59c12cb5bb64c0
Mr. Crank <crank19869@gmail.com>
2021-06-30 16:30:01 +0600

    counter for exists

--- controllers/UrlController.php
+++ controllers/UrlController.php
@@ -16,21 +16,24 @@ class UrlController extends ActiveController
         return filter_var($url, FILTER_VALIDATE_URL);
     }
 
-    private function shuffle(){
+    private function shuffle()
+    {
         $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         return substr(str_shuffle($permitted_chars), 0, 6);
     }
 
-    private function create_short_url($url, $shorturl){
+    private function create_short_url($url, $shorturl)
+    {
         $parse = parse_url($url);
         return "${parse['scheme']}://${parse['host']}/${shorturl}";
     }
 
-    protected function check_url_exists($url) {
+    protected function check_url_exists($url)
+    {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_NOBODY, true);
-        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_exec($ch);
         $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
@@ -53,6 +56,7 @@ class UrlController extends ActiveController
     public function actionCreate()
     {
         $model = new Shorturl();
+
         $url = Yii::$app->request->post('url'); //проверка на нул
 
         if ($this->validateUrlFormat($url) == false) {
@@ -60,23 +64,28 @@ class UrlController extends ActiveController
         }
 
         $count_records = Shorturl::find()
-            ->where(['url'=>$url])
+            ->where(['url' => $url])
             ->one();
-        if(!is_null( $count_records)){
-            return $this->create_short_url($url,$count_records['shorturl']);
+        //var_dump($count_records);die;
+        if (!is_null($count_records)) {
+            $record = Shorturl::findOne($count_records['id']);
+            $record->counter +=1;
+            $count_records['counter'] += 1;
+            $record->save();
+            return $this->create_short_url($url, $count_records['shorturl']);
         }
 
-        if(!$this->check_url_exists($url)){
+        if (!$this->check_url_exists($url)) {
             throw new \yii\web\BadRequestHttpException("URL не существует.");
         }
 
         $model->url = $url;
         $model->shorturl = $this->shuffle();
         $model->creating_date_time = date('Y-m-d H:i:s');
-        $model->counter=1;
+        $model->counter = 1;
         $model->save();
 
-        return $this->create_short_url($url,$model->shorturl);
+        return $this->create_short_url($url, $model->shorturl);
 
     }
 
